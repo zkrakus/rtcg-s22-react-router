423bc1c9c3749677a2377012d3994ca7bf0d9070
zkrakus <zkrakus@gmail.com>
2025-06-13 16:12:49 -0400

    s22-l352:began adding dynamic routes after creating simple routing template. Added NavLinks to persists current nav point.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  // createRoutesFromElements,
+  // Route,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error"
+import ProductDetailPage from "./pages/ProductDetail";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }, { path: '/products', element: <ProductsPage />}]);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage/>} />
+//     <Route path="/products" element={<ProductsPage/>} />
+//   </Route>
+// )
+
+// const router = createBrowserRouter(routeDefinitions);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+      { path: "/products/:productId", element: <ProductDetailPage /> },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,24 @@
-function HomePage(){
+import { Link, useNavigate } from 'react-router-dom'
 
-  return <h1>My Home Page</h1>;
+
+function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate('/products')
+  }
+
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="/products">the list of products.</Link> 
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
